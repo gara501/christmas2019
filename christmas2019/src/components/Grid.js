d4f06ef65ba0247237bb127dad05ae425adeadf4
Jose Chio <jchio@hugeinc.com>
2019-12-16 11:36:07 -0500

    Cube animation on drawer

--- christmas2019/src/components/Grid.js
+++ christmas2019/src/components/Grid.js
@@ -1,17 +1,70 @@
-import React  from 'react';
-import DrawerBox from './DrawerBox';
+import React, { useState, useEffect } from "react";
+import DrawerBox from "./DrawerBox";
 
-const Grid = ({wishes}) => {
+const getWishesBoxes = ({ wishes, setIsShowingMessages }) => {
+  const wishesBoxes = [];
+
+  for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 4; col++) {
+      const flatIndex = col + row * 4;
+      let isLast = false;
+
+      console.log(flatIndex);
+
+      if (flatIndex >= wishes.length) {
+        break;
+      } else if (flatIndex + 1 === wishes.length) {
+        isLast = true;
+      }
+
+      const drawerBox = wishes[flatIndex];
+
+      wishesBoxes.push(
+        <DrawerBox
+          key={drawerBox.id}
+          drawerBox={drawerBox}
+          setIsShowingMessages={setIsShowingMessages}
+          row={row}
+          col={col}
+          isLast={isLast}
+        />
+      );
+    }
+  }
+  return wishesBoxes;
+};
+
+const Grid = ({ wishes }) => {
+  console.log(wishes);
+  const [isShowingMessages, setIsShowingMessages] = useState(false);
+  const [showCubeAnimation, setShowCubeAnimaton] = useState(false);
+
+  let gridClasses = 'grid';
+
+  if (isShowingMessages) {
+    gridClasses += ' grid-showing-messages';
+  }
+
+  if (showCubeAnimation) {
+    gridClasses += ' grid-show-cube-animation';
+  }
+
+  useEffect(() => {
+    if (isShowingMessages) {
+      setTimeout(() => {
+        setShowCubeAnimaton(true);
+      }, 1200)
+    }
+  }, [isShowingMessages]);
+
   return (
-    <div className="grid">
+    <div className={gridClasses}>
       <div className="grid-header"></div>
       <div className="wrapper">
-        { wishes.map( drawerBox => (
-          <DrawerBox key={drawerBox.id} drawerBox={drawerBox} />
-        ))}
+        {getWishesBoxes({ wishes, setIsShowingMessages })}
       </div>
-  </div>
-  )
+    </div>
+  );
 };
- 
+
 export default Grid;
