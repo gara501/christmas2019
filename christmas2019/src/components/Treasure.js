e387adadffca5d1a1636058859f6e4e62794e0ba
Jose Chio <jchio@hugeinc.com>
2019-12-17 14:34:45 -0500

    Add circulito animation poc

--- christmas2019/src/components/Treasure.js
+++ christmas2019/src/components/Treasure.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Treasure = () => (
+const Treasure = () => {
+  const [selectedTreasure, setSelectedTreasure] = useState(false);
+  
+  const onSelectTreasure = () => {
+    const parent = document.querySelector('.paralax-bg');
+    parent.classList.add('selected');
+  }
+
+  return (
   <div className="treasures layer layer9">
-    <img src={process.env.PUBLIC_URL + '/images/treasure.png'} />
+    <img src={process.env.PUBLIC_URL + '/images/treasure.png'} onClick={onSelectTreasure}/>
     <img src={process.env.PUBLIC_URL + '/images/treasure.png'} />
     <img src={process.env.PUBLIC_URL + '/images/treasure.png'} />
   </div>
-);
+)};
  
 export default Treasure;
 
